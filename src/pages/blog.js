215b6f968202b24d79335fb795d8ebde431deb54
shreyapejaz <60458247+shreyapejaz@users.noreply.github.com>
2020-06-20 19:47:55 +0530

    Added thumbnail images for fetching,posting

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,41 +7,54 @@ import {
   CardBody,
 } from 'reactstrap'
 import { Link } from 'gatsby'
-
+import Img from 'gatsby-image'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from 'gatsby'
+import {Row, Col} from 'reactstrap'
 
 const BlogPage = ({ data }) => (
   <Layout>
     <SEO title="Blogs" />
+    <Row>
+    
     {data.allMarkdownRemark.edges.map(post => (
       <div key={post.node.id}>
-         <Card>
-    <CardBody>
-      <CardTitle>
+           
+      <Col md="12">
+      <Card>
       <Link
-        to={post.node.frontmatter.path}>
-        {post.node.frontmatter.title}
-      </Link>
-      </CardTitle>
-      <CardSubtitle>
-        <span className="text-info">{post.node.frontmatter.date}</span> by{' '}
-        <span className="text-info">{post.node.frontmatter.author}</span>
-      </CardSubtitle>
-      <CardText>{post.node.excerpt}</CardText>
-      <Link
-        to={post.node.frontmatter.path}
-        className="btn btn-outline-primary float-right text-uppercase"
-      >
-        Read more
-      </Link>
-    </CardBody>
-  </Card>
+to={post.node.frontmatter.path}>
+<Img className="card-image-top" fluid={post.node.frontmatter.image.childImageSharp.fluid}/>
+</Link>
+<CardBody>
+<CardTitle>
+<Link
+to={post.node.frontmatter.path}>
+{post.node.frontmatter.title}
+</Link>
+</CardTitle>
+<CardSubtitle>
+<span className="text-info">{post.node.frontmatter.date}</span> by{' '}
+<span className="text-info">{post.node.frontmatter.author}</span>
+</CardSubtitle>
+<CardText>{post.node.excerpt}</CardText>
+<Link
+to={post.node.frontmatter.path}
+className="btn btn-outline-primary float-right text-uppercase"
+>
+Read more
+</Link>
+</CardBody>
+</Card>
+</Col>
+    
+         
   </div>
+  
 
     ))}
-
+   </Row>
   </Layout>
 )
 
@@ -56,6 +69,13 @@ export const indexQuery = graphql`
             date
             author
             path
+            image{
+              childImageSharp{
+                fluid(maxWidth: 800){
+                ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
           excerpt
         }
